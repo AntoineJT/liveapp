a8e7eab25c55c8ac1636c95e196431ca47c4a652
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2023-12-28 03:10:06 +0100

    titlebar.js -> titlebar.ts

--- src/titlebar.js
+++
@@ -1,6 +0,0 @@
-import { appWindow } from '@tauri-apps/api/window'
-
-document.getElementById('minimize')
-  .addEventListener('click', () => appWindow.minimize())
-document.getElementById('close')
-  .addEventListener('click', () => appWindow.close())
