4ecf696c1da86f9da50d04bb5731ef086f17f273
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2023-12-29 19:07:00 +0100

    bring back widgetbot view

--- liveapp-react-port/src/App.tsx
+++ liveapp-react-port/src/App.tsx
@@ -1,8 +1,14 @@
 import Titlebar from "./components/Titlebar/Titlebar";
+import WidgetBot from '@widgetbot/react-embed';
+import './App.css'
 
 function App() {
   return <>
     <Titlebar></Titlebar>
+    <WidgetBot className="widgetbot"
+      server="238975753969074177" 
+      channel="718795219369328661"
+    />
   </>;
 }
 
