ab1fe4aa1c6e4998f41852f7589d9fa779a9a842
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2023-12-29 19:22:15 +0100

    improve App code

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import WidgetBot from '@widgetbot/react-embed';
 import './App.css'
 
 function App() {
+  const READ_THE_DOCS_SERVER = "238975753969074177";
+  const CHAT_CHANNEL = "718795219369328661";
+
   return <>
-    <Titlebar></Titlebar>
+    <Titlebar/>
     <WidgetBot className="widgetbot"
-      server="238975753969074177" 
-      channel="718795219369328661"
+      server={READ_THE_DOCS_SERVER}
+      channel={CHAT_CHANNEL}
     />
   </>;
 }
