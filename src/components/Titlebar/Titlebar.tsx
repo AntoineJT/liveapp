23b12a58b68816d7ab7ac8361503fe21edd5a8ed
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2023-12-30 00:40:31 +0100

    add forgotten version from title

--- src/components/Titlebar/Titlebar.tsx
+++ src/components/Titlebar/Titlebar.tsx
@@ -1,13 +1,21 @@
 import './Titlebar.css';
+import { getVersion } from '@tauri-apps/api/app';
 import { appWindow } from '@tauri-apps/api/window';
+import { useEffect, useState } from 'react';
 
 function Titlebar() {
   const minimize = () => appWindow.minimize().catch(console.error);
   const close = () => appWindow.close().catch(console.error);
 
+  const [version, setVersion] = useState('');
+
+  useEffect(() => {
+    getVersion().then(setVersion).catch(console.error);
+  }, []);
+
   return (
     <header data-tauri-drag-region className="titlebar">
-      <h1 data-tauri-drag-region>Live App</h1>
+      <h1 data-tauri-drag-region>Live App v{version}</h1>
       <section className="buttons">
         <button id="minimize" onClick={minimize}>
           <img src="src/assets/fontawesome5/minimize.svg" alt="minimize window" />
