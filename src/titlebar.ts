725a8ea2ad25cd1b6e502a4b3bcafd14101da117
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2023-12-29 02:43:52 +0100

    broken adjust feature
    recompute window size & position on the screen
    (useful when using multiple screens)

--- src/titlebar.ts
+++ src/titlebar.ts
@@ -1,6 +1,12 @@
+import { emit } from '@tauri-apps/api/event'
 import { appWindow } from '@tauri-apps/api/window'
 
 document!.getElementById('minimize')!
-  .addEventListener('click', () => appWindow.minimize())
+  .addEventListener('click', 
+    () => appWindow.minimize().catch(console.error))
 document!.getElementById('close')!
-  .addEventListener('click', () => appWindow.close())
+  .addEventListener('click', 
+    () => appWindow.close().catch(console.error))
+document!.getElementById('adjust')!
+  .addEventListener('click', 
+    () => emit('adjust').catch(console.error))
